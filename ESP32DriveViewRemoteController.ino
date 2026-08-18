2f17a1eb3eb1cb9c19f062903f6d0372ceed1c06
Codexzier <johannes.paul.langner@live.de>
2026-08-18 23:13:55 +0200

    it runs with jpeg

--- ESP32DriveViewRemoteController.ino
+++ ESP32DriveViewRemoteController.ino
@@ -38,14 +38,14 @@ const char* serverIP = "192.168.4.2";     // IP of ESP32-Server
 const uint16_t serverPort = 5001;         // Port of Servers
 WiFiClient _client;
 
-// ========================================================================================
+
+
+// ==================================================
 // Display GC9A01A
 
 // libraries for TFT Display
 #include "GC9A01_LTSM.hpp"
 #include "fonts_LTSM/FontArialBold_LTSM.hpp" // 16x16 pixels
-
-
 
 // Pin Mapping XIAO ESP32-S3
 #define TFT_CS     D0 // Cable Select
@@ -65,6 +65,18 @@ const uint16_t mColorOff = mTft.C_DCYAN;
 
 uint8_t imageBuffer[IMAGE_SIZE];
 size_t receivedBytes = 0;
+
+// ==================================================
+// jpeg
+#include <TJpg_Decoder.h>
+
+// Callback-Funktion für TJpg_Decoder
+bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) {
+  //Serial.println("callback function for TJeg!");
+  mTft.drawBitmap16Data(x, y, (uint8_t*)bitmap, w, h);
+  
+  return true;
+}
 
 long _timeoutForReconnect = 30;
 long _lastmillis = 0;
@@ -125,6 +137,14 @@ void setup() {
   mTft.writeBuffer();
   delay(1000);
 
+  TJpgDec.setJpgScale(1);
+  
+  TJpgDec.setSwapBytes(true);
+
+  TJpgDec.setCallback(tft_output);
+  delay(1000);
+
+
   connectToServer();
 
   mTft.setCursor(40, 90);
@@ -140,21 +160,66 @@ void loop() {
     Serial.println("connected");
     digitalWrite(LED_BUILTIN, true);
 
-    if (_client.available()) {     // if there's bytes to read from the client
+    if (_client.available() >= sizeof(uint32_t)) {     // if there's bytes to read from the client
+
+       // 1. Bildgröße empfangen
+      uint8_t sizeBytes[4];
+      _client.read(sizeBytes, 4);  // 4 Bytes empfangen
+      uint32_t jpgSize = (sizeBytes[0] << 24) |
+                  (sizeBytes[1] << 16) |
+                  (sizeBytes[2] << 8)  |
+                  sizeBytes[3];  // Big-Endian zu uint32_t umwandeln
 
       Serial.println("read transfer");
       // Lies die verfügbaren Bytes in den Puffer
-      size_t bytesToRead = min((size_t)_client.available(), IMAGE_SIZE - receivedBytes);
-      size_t bytesRead = _client.read(&imageBuffer[receivedBytes], bytesToRead);
-      receivedBytes += bytesRead;
+      // size_t bytesToRead = min((size_t)_client.available(), IMAGE_SIZE - receivedBytes);
+      // size_t bytesRead = _client.read(&imageBuffer[receivedBytes], bytesToRead);
+      // receivedBytes += bytesRead;
+      // 2. JPEG-Daten empfangen
+      //uint8_t *jpgBuffer = (uint8_t *)malloc(jpgSize);
+      //size_t received = 0;
+
+      
+
+      // while (received < jpgSize) {
+      //   if (_client.available()) {
+      //     received += _client.read(jpgBuffer + received, jpgSize - received);
+      //   }
+      //   sprintf(buffer, "%d", received);
+      //   Serial.print("received "); Serial.println(buffer);
+      //   delay(1);
+      // }
+      // 2. JPEG-Daten empfangen
+  uint8_t *jpgBuffer = (uint8_t *)malloc(jpgSize);
+  size_t received = 0;
+
+  while (received < jpgSize) {
+    if (_client.available()) {
+      received += _client.read(jpgBuffer + received, jpgSize - received);
+      
+    }
+    delay(1);
+  }
+
+  char buffer[12];
+      sprintf(buffer, "%d", jpgSize);
+      Serial.print("jpg size "); Serial.println(buffer);
+  sprintf(buffer, "%d", received);
+      Serial.print("received "); Serial.println(buffer);
+
+      TJpgDec.drawJpg(0, 0, jpgBuffer, jpgSize);
+free(jpgBuffer);
+    Serial.println("Has write to draw jpg. Wait for Callback");
+    mTft.writeBuffer();
 
       // Wenn das gesamte Bild empfangen wurde
-      if (receivedBytes == IMAGE_SIZE) {
+      //if (receivedBytes == IMAGE_SIZE) {
+        
         // Hier: Puffer an GC9A01_LTSM übergeben
-        mTft.drawBitmap16Data(0, 0, (uint8_t *)imageBuffer, 240, 240);
-        receivedBytes = 0;  // Zurücksetzen für nächstes Bild
-        mTft.writeBuffer();
-      }
+        //mTft.drawBitmap16Data(0, 0, (uint8_t *)imageBuffer, 240, 240);
+        //receivedBytes = 0;  // Zurücksetzen für nächstes Bild
+        //mTft.writeBuffer();
+      //}
     }
 
     digitalWrite(LED_BUILTIN, false);
